ab3650d5ba8943ee59db824672b48c637736477c
marcusemanuel1323 <82175709+marcusemanuel1323@users.noreply.github.com>
2022-04-27 02:18:13 -0300

    Update script.js

--- AMAL/script.js
+++ AMAL/script.js
@@ -1,12 +1,3 @@
-const menuBar = document.getElementById('btn-mobile');
-
-function toggleMenu(){
-    const menuMobile = document.getElementById('menu-mobile');
-    menuMobile.classList.toggle('active');
-}
-
-menuBar.addEventListener('click', toggleMenu);
-
 $('#btn-mobile').click(function(){
     $('#menu-mobile').toggleClass('active');
 });
